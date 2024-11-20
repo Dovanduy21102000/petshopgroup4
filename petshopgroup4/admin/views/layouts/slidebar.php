f2ed93188eeb4dccbe11a63343f57c1a88498d54
Doduy2110 <duydvph46536@fpt.edu.vn>
2024-11-21 02:32:06 +0700

    update

--- petshopgroup4/admin/views/layouts/slidebar.php
+++ petshopgroup4/admin/views/layouts/slidebar.php
@@ -2,7 +2,7 @@
   <!-- Brand Logo -->
   <a href="../../index3.html" class="brand-link text-center">
     <img src="./assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">Admin</span>
+    <span class="brand-text font-weight-light">ADMIN DASHBOARD</span>
   </a>
 
   <!-- Sidebar -->
@@ -11,7 +11,7 @@
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <img src="./assets/dist/img/AdminLTELogo.png" class="brand-image img-circle elevation-3" style="opacity: .8">
       <div class="info">
-        <a href="#" class="d-block">Duck ngo</a>
+        <a href="#" class="d-block">Đỗ Duy</a>
       </div>
     </div>
 
@@ -25,7 +25,7 @@
           <a href="<?= BASE_URL_ADMIN  ?>" class="nav-link">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>
-              TRang chủ
+              HOME
             </p>
           </a>
         </li>
